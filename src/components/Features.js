86a03e8abbd1d0a46c228e138bac63059cd323be
Vinay <vinay86bhamre@gmail.com>
2022-11-15 19:33:50 +0530

    alt attribute added to img tags

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
 // import features data
-import { featuresData } from '../data';
+import { featuresData } from "../data";
 
 // import icons
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight } from "react-icons/bs";
 
 const Features = () => {
   // destructure features data
   const { title, subtitle, list } = featuresData;
   return (
-    <section className='my-[70px] xl:my-[150px]'>
-      <div className='container mx-auto'>
+    <section className="my-[70px] xl:my-[150px]">
+      <div className="container mx-auto">
         {/* text */}
-        <div className='text-center'>
+        <div className="text-center">
           <h2
-            className='h2 mb-6 xl:mb-12'
-            data-aos='fade-down'
-            data-aos-delay='100'
+            className="h2 mb-6 xl:mb-12"
+            data-aos="fade-down"
+            data-aos-delay="100"
           >
             {title}
           </h2>
           <p
-            className='lead max-w-[584px] mx-auto mb-16 xl:mb-24'
-            data-aos='fade-down'
-            data-aos-delay='200'
+            className="lead max-w-[584px] mx-auto mb-16 xl:mb-24"
+            data-aos="fade-down"
+            data-aos-delay="200"
           >
             {subtitle}
           </p>
         </div>
         {/* feature list */}
-        <div className='grid grid-cols-1 gap-[50px] xl:grid-cols-2'>
+        <div className="grid grid-cols-1 gap-[50px] xl:grid-cols-2">
           {list.map((feature, index) => {
             // destructure feature
             const { image, bgImage, title, description, linkText, delay } =
@@ -39,34 +39,34 @@ const Features = () => {
             return (
               <div
                 key={index}
-                className='w-full max-w-[530px] h-[358px] relative flex flex-col items-center justify-center xl:flex-row xl:justify-start mx-auto'
-                data-aos='zoom-in'
-                data-aos-offset='100'
+                className="w-full max-w-[530px] h-[358px] relative flex flex-col items-center justify-center xl:flex-row xl:justify-start mx-auto"
+                data-aos="zoom-in"
+                data-aos-offset="100"
                 data-aos-delay={delay}
               >
                 {/* bg image */}
-                <div className='hidden xl:flex absolute top-0 right-0 -z-10'>
-                  <img src={bgImage} />
+                <div className="hidden xl:flex absolute top-0 right-0 -z-10">
+                  <img src={bgImage} alt="feature image" />
                 </div>
 
                 {/* icon image */}
                 <div
-                  className='max-w-[120px] xl:mr-7 xl:max-w-[232px]'
-                  data-aos='zoom-in-right'
+                  className="max-w-[120px] xl:mr-7 xl:max-w-[232px]"
+                  data-aos="zoom-in-right"
                   data-aos-delay={delay}
                 >
-                  <img src={image} />
+                  <img src={image} alt="icon image" />
                 </div>
                 {/* text */}
-                <div className='max-w-[220px]'>
-                  <h3 className='h3 mb-4'>{title}</h3>
-                  <p className='font-light italic mb-4'>{description}</p>
+                <div className="max-w-[220px]">
+                  <h3 className="h3 mb-4">{title}</h3>
+                  <p className="font-light italic mb-4">{description}</p>
                   {/* link & arrow */}
-                  <div className='flex items-center gap-x-2 group'>
-                    <a className='text-primary font-bold' href='#'>
+                  <div className="flex items-center gap-x-2 group">
+                    <a className="text-primary font-bold" href="#learnmore">
                       {linkText}
                     </a>
-                    <BsArrowRight className='text-xl text-accent-primary group-hover:ml-[5px] transition-all' />
+                    <BsArrowRight className="text-xl text-accent-primary group-hover:ml-[5px] transition-all" />
                   </div>
                 </div>
               </div>
--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import React from "react";
 
 // import testimonials data
-import { testimonialsData } from '../data';
+import { testimonialsData } from "../data";
 
 // import swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // import swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import '../slider.css';
+import "swiper/css";
+import "swiper/css/pagination";
+import "../slider.css";
 
 // import required modules
-import { Pagination } from 'swiper';
+import { Pagination } from "swiper";
 
 const TestimonialSlider = () => {
   return (
     <Swiper
-      slidesPerView={'auto'}
+      slidesPerView={"auto"}
       centeredSlides={true}
       spaceBetween={20}
       pagination={{
         clickable: true,
       }}
       modules={[Pagination]}
-      className='mySwiper'
+      className="mySwiper"
     >
       {testimonialsData.map((slide, index) => {
         // destructure slide
-        const { image, message, name, web, delay } = slide;
+        // const { image, message, name, web, delay } = slide;
+        const { image, message, name, web } = slide;
         return (
           // slide
           <SwiperSlide
             key={index}
-            className='bg-white rounded-[20px] border border-accent-primary xl:max-w-[645px] max-h-[330px] pt-[60px] px-[35px] xl:px-[70px] pb-[50px] flex items-start gap-x-[30px] shadow-xl'
+            className="bg-white rounded-[20px] border border-accent-primary xl:max-w-[645px] max-h-[330px] pt-[60px] px-[35px] xl:px-[70px] pb-[50px] flex items-start gap-x-[30px] shadow-xl"
           >
             {/* avatar image */}
-            <img src={image} alt='' />
+            <img src={image} alt="" />
             {/* text */}
             <div>
-              <div className='text-lg text-primary font-bold'>{name}</div>
-              <div className='mb-4 font-semibold text-accent-primary'>
+              <div className="text-lg text-primary font-bold">{name}</div>
+              <div className="mb-4 font-semibold text-accent-primary">
                 {web}
               </div>
-              <p className='max-w-[340px]'>{message}</p>
+              <p className="max-w-[340px]">{message}</p>
             </div>
           </SwiperSlide>
         );
